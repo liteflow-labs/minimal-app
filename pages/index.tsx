1bb77bf40aa891f535fc191a3a041742e5ca2178
Nicolas Mahe <nicolas@mahe.me>
2022-10-05 12:03:54 +0700

    fix lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,30 @@
-import { Signer, TypedDataSigner } from "@ethersproject/abstract-signer";
-import { useCreateOffer } from "@nft/hooks";
-import { BigNumber } from "ethers";
-import { useCallback } from "react";
-import { useSigner } from "wagmi";
-import styles from "../styles/app.module.css";
+import { Signer, TypedDataSigner } from '@ethersproject/abstract-signer'
+import { useCreateOffer } from '@nft/hooks'
+import { BigNumber } from 'ethers'
+import { useCallback } from 'react'
+import { useSigner } from 'wagmi'
+import styles from '../styles/app.module.css'
 
 export default function Home() {
-  const { data: signer } = useSigner();
+  const { data: signer } = useSigner()
   const [_create] = useCreateOffer(
-    signer as (Signer & TypedDataSigner) | undefined
-  );
+    signer as (Signer & TypedDataSigner) | undefined,
+  )
   const assetId =
-    "80001-0x7c68c3c59ceb245733a2fdeb47f5f7d6dbcc65b3-60249402084937876423066029128237587855293854847399126863606291191289075471730";
+    '80001-0x7c68c3c59ceb245733a2fdeb47f5f7d6dbcc65b3-60249402084937876423066029128237587855293854847399126863606291191289075471730'
 
   const create = useCallback(async () => {
     const amount = parseFloat(prompt('amount in USDC'))
     const id = await _create({
-      type: "BUY",
+      type: 'BUY',
       assetId,
-      currencyId: "80001-0x0fa8781a83e46826621b3bc094ea2a0212e71b23", // USDC on Polygon Mumbai
+      currencyId: '80001-0x0fa8781a83e46826621b3bc094ea2a0212e71b23', // USDC on Polygon Mumbai
       expiredAt: new Date(Date.now() + 1000 * 60 * 60),
       quantity: BigNumber.from(1),
       unitPrice: BigNumber.from(amount * 1e6),
-    });
-    alert(id);
-  }, [_create]);
+    })
+    alert(id)
+  }, [_create])
 
   return (
     <>
@@ -42,5 +42,5 @@ export default function Home() {
         Check bids
       </a>
     </>
-  );
+  )
 }
